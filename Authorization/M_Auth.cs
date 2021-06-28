ab27e733e313c0895b247729064a962e4f8996c8
Battle Mercy <battle.mercy@yandex.ru>
2021-06-28 10:52:38 +0700

    Добавил локальную базу данных, создал там таблицу User, создал модель базы данных, отредактировал метод авторизации.

--- Authorization/M_Auth.cs
+++ Authorization/M_Auth.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Authorization
 {
     public class M_Auth
     {
+        public bool Enter(string login, string password)
+        {
+            Database1Entities db = new Database1Entities();
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Вы не заполнили все поля", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            var auth_check = db.User.FirstOrDefault(ch => ch.Login == login && ch.Password == password);
+            if (auth_check == null)
+            {
+                MessageBox.Show("Логин или пароль введены не верно", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
     }
 }
